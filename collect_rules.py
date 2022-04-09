82fc9c25974d9ef10887a006f3bab3054d73aeec
Zheng Tang <oscartang1120@gmail.com>
2022-04-09 03:18:07 -0700

    Update collect_rules.py

--- collect_rules.py
+++ collect_rules.py
@@ -52,10 +52,10 @@ def rules_with_out_golds(candidates, origin, model_output):
         postags = ['ROOT'] + origin[i]['stanford_pos']
         
         if item['predicted_label'] != 'no_relation':#== item['gold_label']:#
-            if len(item['predicted_tags']) != 0 and len(item['gold_tags']) == 0:
-                subj = list(range(origin[i]['subj_start'], origin[i]['subj_end']+1))
-                obj = list(range(origin[i]['obj_start'], origin[i]['obj_end']+1))
-                triggers = [j for j, w in enumerate(origin[i]['token']) if j in item['predicted_tags'] and j not in subj and j not in obj]
+            if len(item['predicted_tags']) != 0:
+                subj = list(range(origin[i]['subj_start']+1, origin[i]['subj_end']+2))
+                obj = list(range(origin[i]['obj_start']+1, origin[i]['obj_end']+2))
+                triggers = [j for j, w in enumerate(origin[i]['token']) if j in item['predicted_tags'] and j+1 not in subj and j+1 not in obj]
                 if triggers:
                     sp = []
                     op = []
@@ -116,7 +116,7 @@ def rules_with_corrects(candidates, origin, model_output):
             if len(item['predicted_tags']) != 0 and len(item['gold_tags']) == 0:
                 subj = list(range(origin[i]['subj_start']+1, origin[i]['subj_end']+2))
                 obj = list(range(origin[i]['obj_start']+1, origin[i]['obj_end']+2))
-                triggers = [j+1 for j, w in enumerate(origin[i]['token']) if j in item['predicted_tags'] and j not in subj and j not in obj]
+                triggers = [j+1 for j, w in enumerate(origin[i]['token']) if j in item['predicted_tags'] and j+1 not in subj and j+1 not in obj]
                 if triggers:
                     sp = []
                     op = []
@@ -198,14 +198,14 @@ def save_rule_dict(candidates, subjects, objects, name):
 
 if __name__ == "__main__":
 
-    model_output = json.load(open('output_132_test_best_model_10.json'))
+    model_output = json.load(open('output_665_test_best_model_9.json'))
     origin = json.load(open('/Users/zheng/Documents/GitHub/syn-GCN/tacred/data/json/test.json'))
 
     candidates = defaultdict(list)
 
-    candidates, subjects, objects = rules_with_corrects(candidates, origin, model_output)
+    candidates, subjects, objects = rules_with_out_golds(candidates, origin, model_output)
 
-    save_rule_dict(candidates, subjects, objects, "test_pred")
+    save_rule_dict(candidates, subjects, objects, "test")
 
 
 
